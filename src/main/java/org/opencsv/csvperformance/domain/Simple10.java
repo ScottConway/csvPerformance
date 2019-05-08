9936b2cb2faad7b0ae39724e8d30e7533ec0e7b8
Scott Conway <scott.conway@gmail.com>
2019-05-07 22:58:00 -0500

    Added first domain class.

---
+++ src/main/java/org/opencsv/csvperformance/domain/Simple10.java
@@ -0,0 +1,96 @@
+package org.opencsv.csvperformance.domain;
+
+import org.joda.time.DateTime;
+
+public class Simple10 {
+    private String text1;
+    private int num1;
+    private double double1;
+    private DateTime dateTime1;
+    private char char1;
+    private String text2;
+    private int num2;
+    private double double2;
+    private DateTime dateTime2;
+    private char char2;
+
+    public String getText1() {
+        return text1;
+    }
+
+    public void setText1(String text1) {
+        this.text1 = text1;
+    }
+
+    public int getNum1() {
+        return num1;
+    }
+
+    public void setNum1(int num1) {
+        this.num1 = num1;
+    }
+
+    public double getDouble1() {
+        return double1;
+    }
+
+    public void setDouble1(double double1) {
+        this.double1 = double1;
+    }
+
+    public DateTime getDateTime1() {
+        return dateTime1;
+    }
+
+    public void setDateTime1(DateTime dateTime1) {
+        this.dateTime1 = dateTime1;
+    }
+
+    public char getChar1() {
+        return char1;
+    }
+
+    public void setChar1(char char1) {
+        this.char1 = char1;
+    }
+
+    public String getText2() {
+        return text2;
+    }
+
+    public void setText2(String text2) {
+        this.text2 = text2;
+    }
+
+    public int getNum2() {
+        return num2;
+    }
+
+    public void setNum2(int num2) {
+        this.num2 = num2;
+    }
+
+    public double getDouble2() {
+        return double2;
+    }
+
+    public void setDouble2(double double2) {
+        this.double2 = double2;
+    }
+
+    public DateTime getDateTime2() {
+        return dateTime2;
+    }
+
+    public void setDateTime2(DateTime dateTime2) {
+        this.dateTime2 = dateTime2;
+    }
+
+    public char getChar2() {
+        return char2;
+    }
+
+    public void setChar2(char char2) {
+        this.char2 = char2;
+    }
+}
