519affca974d39fd6cf887717a12e558c57d73a4
Scott Conway <scott.conway@gmail.com>
2019-05-07 20:29:17 -0500

    generated spring boot project.

---
+++ src/main/java/org/opencsv/csvperformance/CsvPerformanceApplication.java
@@ -0,0 +1,13 @@
+package org.opencsv.csvperformance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvPerformanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CsvPerformanceApplication.class, args);
+    }
+
+}
