f4973c2a1610784a30bbf7c24d4a4bcc77341240
Scott Conway <scott.conway@gmail.com>
2019-05-12 10:34:13 -0500

    Removed old start page.  Added Simple50

---
+++ src/main/java/org/opencsv/csvperformance/domain/Simple50.java
@@ -0,0 +1,456 @@
+package org.opencsv.csvperformance.domain;
+
+import org.joda.time.DateTime;
+
+public class Simple50 {
+    private String text1;
+    private int num1;
+    private double double1;
+    private DateTime dateTime1;
+    private char char1;
+    private String text2;
+    private int num2;
+    private double double2;
+    private DateTime dateTime2;
+    private char char2;
+    private String text3;
+    private int num3;
+    private double double3;
+    private DateTime dateTime3;
+    private char char3;
+    private String text4;
+    private int num4;
+    private double double4;
+    private DateTime dateTime4;
+    private char char4;
+    private String text5;
+    private int num5;
+    private double double5;
+    private DateTime dateTime5;
+    private char char5;
+    private String text6;
+    private int num6;
+    private double double6;
+    private DateTime dateTime6;
+    private char char6;
+    private String text7;
+    private int num7;
+    private double double7;
+    private DateTime dateTime7;
+    private char char7;
+    private String text8;
+    private int num8;
+    private double double8;
+    private DateTime dateTime8;
+    private char char8;
+    private String text9;
+    private int num9;
+    private double double9;
+    private DateTime dateTime9;
+    private char char9;
+    private String text10;
+    private int num10;
+    private double double10;
+    private DateTime dateTime10;
+    private char char10;
+
+    public String getText1() {
+        return text1;
+    }
+
+    public void setText1(String text1) {
+        this.text1 = text1;
+    }
+
+    public int getNum1() {
+        return num1;
+    }
+
+    public void setNum1(int num1) {
+        this.num1 = num1;
+    }
+
+    public double getDouble1() {
+        return double1;
+    }
+
+    public void setDouble1(double double1) {
+        this.double1 = double1;
+    }
+
+    public DateTime getDateTime1() {
+        return dateTime1;
+    }
+
+    public void setDateTime1(DateTime dateTime1) {
+        this.dateTime1 = dateTime1;
+    }
+
+    public char getChar1() {
+        return char1;
+    }
+
+    public void setChar1(char char1) {
+        this.char1 = char1;
+    }
+
+    public String getText2() {
+        return text2;
+    }
+
+    public void setText2(String text2) {
+        this.text2 = text2;
+    }
+
+    public int getNum2() {
+        return num2;
+    }
+
+    public void setNum2(int num2) {
+        this.num2 = num2;
+    }
+
+    public double getDouble2() {
+        return double2;
+    }
+
+    public void setDouble2(double double2) {
+        this.double2 = double2;
+    }
+
+    public DateTime getDateTime2() {
+        return dateTime2;
+    }
+
+    public void setDateTime2(DateTime dateTime2) {
+        this.dateTime2 = dateTime2;
+    }
+
+    public char getChar2() {
+        return char2;
+    }
+
+    public void setChar2(char char2) {
+        this.char2 = char2;
+    }
+
+    public String getText3() {
+        return text3;
+    }
+
+    public void setText3(String text3) {
+        this.text3 = text3;
+    }
+
+    public int getNum3() {
+        return num3;
+    }
+
+    public void setNum3(int num3) {
+        this.num3 = num3;
+    }
+
+    public double getDouble3() {
+        return double3;
+    }
+
+    public void setDouble3(double double3) {
+        this.double3 = double3;
+    }
+
+    public DateTime getDateTime3() {
+        return dateTime3;
+    }
+
+    public void setDateTime3(DateTime dateTime3) {
+        this.dateTime3 = dateTime3;
+    }
+
+    public char getChar3() {
+        return char3;
+    }
+
+    public void setChar3(char char3) {
+        this.char3 = char3;
+    }
+
+    public String getText4() {
+        return text4;
+    }
+
+    public void setText4(String text4) {
+        this.text4 = text4;
+    }
+
+    public int getNum4() {
+        return num4;
+    }
+
+    public void setNum4(int num4) {
+        this.num4 = num4;
+    }
+
+    public double getDouble4() {
+        return double4;
+    }
+
+    public void setDouble4(double double4) {
+        this.double4 = double4;
+    }
+
+    public DateTime getDateTime4() {
+        return dateTime4;
+    }
+
+    public void setDateTime4(DateTime dateTime4) {
+        this.dateTime4 = dateTime4;
+    }
+
+    public char getChar4() {
+        return char4;
+    }
+
+    public void setChar4(char char4) {
+        this.char4 = char4;
+    }
+
+    public String getText5() {
+        return text5;
+    }
+
+    public void setText5(String text5) {
+        this.text5 = text5;
+    }
+
+    public int getNum5() {
+        return num5;
+    }
+
+    public void setNum5(int num5) {
+        this.num5 = num5;
+    }
+
+    public double getDouble5() {
+        return double5;
+    }
+
+    public void setDouble5(double double5) {
+        this.double5 = double5;
+    }
+
+    public DateTime getDateTime5() {
+        return dateTime5;
+    }
+
+    public void setDateTime5(DateTime dateTime5) {
+        this.dateTime5 = dateTime5;
+    }
+
+    public char getChar5() {
+        return char5;
+    }
+
+    public void setChar5(char char5) {
+        this.char5 = char5;
+    }
+
+    public String getText6() {
+        return text6;
+    }
+
+    public void setText6(String text6) {
+        this.text6 = text6;
+    }
+
+    public int getNum6() {
+        return num6;
+    }
+
+    public void setNum6(int num6) {
+        this.num6 = num6;
+    }
+
+    public double getDouble6() {
+        return double6;
+    }
+
+    public void setDouble6(double double6) {
+        this.double6 = double6;
+    }
+
+    public DateTime getDateTime6() {
+        return dateTime6;
+    }
+
+    public void setDateTime6(DateTime dateTime6) {
+        this.dateTime6 = dateTime6;
+    }
+
+    public char getChar6() {
+        return char6;
+    }
+
+    public void setChar6(char char6) {
+        this.char6 = char6;
+    }
+
+    public String getText7() {
+        return text7;
+    }
+
+    public void setText7(String text7) {
+        this.text7 = text7;
+    }
+
+    public int getNum7() {
+        return num7;
+    }
+
+    public void setNum7(int num7) {
+        this.num7 = num7;
+    }
+
+    public double getDouble7() {
+        return double7;
+    }
+
+    public void setDouble7(double double7) {
+        this.double7 = double7;
+    }
+
+    public DateTime getDateTime7() {
+        return dateTime7;
+    }
+
+    public void setDateTime7(DateTime dateTime7) {
+        this.dateTime7 = dateTime7;
+    }
+
+    public char getChar7() {
+        return char7;
+    }
+
+    public void setChar7(char char7) {
+        this.char7 = char7;
+    }
+
+    public String getText8() {
+        return text8;
+    }
+
+    public void setText8(String text8) {
+        this.text8 = text8;
+    }
+
+    public int getNum8() {
+        return num8;
+    }
+
+    public void setNum8(int num8) {
+        this.num8 = num8;
+    }
+
+    public double getDouble8() {
+        return double8;
+    }
+
+    public void setDouble8(double double8) {
+        this.double8 = double8;
+    }
+
+    public DateTime getDateTime8() {
+        return dateTime8;
+    }
+
+    public void setDateTime8(DateTime dateTime8) {
+        this.dateTime8 = dateTime8;
+    }
+
+    public char getChar8() {
+        return char8;
+    }
+
+    public void setChar8(char char8) {
+        this.char8 = char8;
+    }
+
+    public String getText9() {
+        return text9;
+    }
+
+    public void setText9(String text9) {
+        this.text9 = text9;
+    }
+
+    public int getNum9() {
+        return num9;
+    }
+
+    public void setNum9(int num9) {
+        this.num9 = num9;
+    }
+
+    public double getDouble9() {
+        return double9;
+    }
+
+    public void setDouble9(double double9) {
+        this.double9 = double9;
+    }
+
+    public DateTime getDateTime9() {
+        return dateTime9;
+    }
+
+    public void setDateTime9(DateTime dateTime9) {
+        this.dateTime9 = dateTime9;
+    }
+
+    public char getChar9() {
+        return char9;
+    }
+
+    public void setChar9(char char9) {
+        this.char9 = char9;
+    }
+
+    public String getText10() {
+        return text10;
+    }
+
+    public void setText10(String text10) {
+        this.text10 = text10;
+    }
+
+    public int getNum10() {
+        return num10;
+    }
+
+    public void setNum10(int num10) {
+        this.num10 = num10;
+    }
+
+    public double getDouble10() {
+        return double10;
+    }
+
+    public void setDouble10(double double10) {
+        this.double10 = double10;
+    }
+
+    public DateTime getDateTime10() {
+        return dateTime10;
+    }
+
+    public void setDateTime10(DateTime dateTime10) {
+        this.dateTime10 = dateTime10;
+    }
+
+    public char getChar10() {
+        return char10;
+    }
+
+    public void setChar10(char char10) {
+        this.char10 = char10;
+    }
+}
