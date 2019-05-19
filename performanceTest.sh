af6b527d14b16ff0300c4e45fca7cf2baf9af196
Scott Conway <scott.conway@gmail.com>
2019-05-18 22:54:30 -0500

    Created Script to run stand alone.
    Cleaned up result html files.

---
+++ performanceTest.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+MEMORY_OPTS="-Xms256m -Xmx512m -XX:MaxMetaspaceSize=256m"
+
+GC_OPTS="-XX:+UseG1GC -Xlog:gc=info:file=target/gc.log -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=target/java_pid<pid>.hprof -XX:+UseGCOverheadLimit"
+
+JAR_NAME="target/csv-performance-0.0.1-SNAPSHOT.jar"
+
+java -jar $MEMORY_OPTS $GC_OPTS $JAR_NAME
