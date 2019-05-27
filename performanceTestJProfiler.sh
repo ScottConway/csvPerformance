feda77e37ac4f27f6964b791d0a0b0626a928f30
Scott Conway <scott.conway@gmail.com>
2019-05-26 23:31:53 -0500

    Added JProfiler performance script.

---
+++ performanceTestJProfiler.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+MEMORY_OPTS="-Xms256m -Xmx512m -XX:MaxMetaspaceSize=256m"
+
+GC_OPTS="-XX:+UseG1GC -Xlog:gc=info:file=target/gc.log -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=target/java_pid<pid>.hprof -XX:+UseGCOverheadLimit"
+
+JAR_NAME="target/csv-performance-0.2-SNAPSHOT.jar"
+
+JPROFILER_OPTS="-Xshare:off"
+
+java -jar $MEMORY_OPTS $GC_OPTS $JPROFILER_OPTS $JAR_NAME
