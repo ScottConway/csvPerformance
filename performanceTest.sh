aac89d896b229b53a02cb14da4a2eaaef3842afc
Scott Conway <scott.conway@gmail.com>
2019-10-22 10:29:13 -0500

    Updated to use opencsv:5.0

--- performanceTest.sh
+++ performanceTest.sh
@@ -4,6 +4,6 @@ MEMORY_OPTS="-Xms256m -Xmx512m -XX:MaxMetaspaceSize=256m"
 
 GC_OPTS="-XX:+UseG1GC -Xlog:gc=info:file=target/gc.log -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=target/java_pid<pid>.hprof -XX:+UseGCOverheadLimit"
 
-JAR_NAME="target/csv-performance-0.2-SNAPSHOT.jar"
+JAR_NAME="target/csv-performance-0.3-SNAPSHOT.jar"
 
 java -jar $MEMORY_OPTS $GC_OPTS $JAR_NAME
--- performanceTestJProfiler.sh
+++ performanceTestJProfiler.sh
@@ -4,7 +4,7 @@ MEMORY_OPTS="-Xms256m -Xmx512m -XX:MaxMetaspaceSize=256m"
 
 GC_OPTS="-XX:+UseG1GC -Xlog:gc=info:file=target/gc.log -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=target/java_pid<pid>.hprof -XX:+UseGCOverheadLimit"
 
-JAR_NAME="target/csv-performance-0.2-SNAPSHOT.jar"
+JAR_NAME="target/csv-performance-0.3-SNAPSHOT.jar"
 
 JPROFILER_OPTS="-Xshare:off"
 
