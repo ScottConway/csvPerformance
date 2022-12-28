5c270bae9369f7f5aeab98cbb96a3fb9adabd5ac
scottconway <scott.conway@gmail.com>
2022-12-28 17:32:05 -0600

    touch files at beginning to have something to load (until I find a good front end file chooser that will create a file).

--- performanceTestJProfiler.sh
+++ performanceTestJProfiler.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+touch rfc4180ParserA10.csv
+touch rfc4180ParserA50.csv
+touch rfc4180ParserS10.csv
+touch rfc4180ParserS50.csv
+touch csvParserA10.csv
+touch csvParserA50.csv
+touch csvParserS10.csv
+touch csvParserS50.csv
+
 MEMORY_OPTS="-Xms384m -Xmx768m -XX:MaxMetaspaceSize=256m"
 
 GC_OPTS="-XX:+UseG1GC -Xlog:gc:target/gc.log -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=target/java_pid<pid>.hprof -XX:+UseGCOverheadLimit"
