4aad439eed68ec862dc803ae34f15ddc402a84aa
scottconway <scott.conway@gmail.com>
2022-12-28 15:08:38 -0600

    updated garbage collection options in test scripts to remove deprecation warning.

--- performanceTestJProfiler.sh
+++ performanceTestJProfiler.sh
@@ -2,10 +2,7 @@
 
 MEMORY_OPTS="-Xms384m -Xmx768m -XX:MaxMetaspaceSize=256m"
 
-#java9
-#GC_OPTS="-XX:+UseG1GC -Xlog:gc=info:file=target/gc.log -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=target/java_pid<pid>.hprof -XX:+UseGCOverheadLimit"
-#java8
-GC_OPTS="-XX:+UseG1GC -Xloggc:target/gc.log -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=target/java_pid<pid>.hprof -XX:+UseGCOverheadLimit"
+GC_OPTS="-XX:+UseG1GC -Xlog:gc:target/gc.log -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=target/java_pid<pid>.hprof -XX:+UseGCOverheadLimit"
 
 JAR_NAME="target/csv-performance-0.5-SNAPSHOT.jar"
 
